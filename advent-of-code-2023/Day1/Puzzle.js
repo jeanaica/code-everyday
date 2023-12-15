f7346ad2dd8fe0a4dcbfa7614756c42ff19c5817
Jeanaica Suplido-Alinsub <sjeanaica@gmail.com>
2023-12-15 14:16:11 +0800

    🦺 Solve Day 1 puzzle 2

--- advent-of-code-2023/Day1/Puzzle.js
+++ advent-of-code-2023/Day1/Puzzle.js
@@ -5,30 +5,59 @@ const rl = readline.createInterface({
 });
 
 let lines = [];
+const wordPattern = new RegExp(
+  /(?:one|two|three|four|five|six|seven|eight|nine|\d)/g
+);
+
+const numberWords = {
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9',
+};
+
+function wordToNumValue(inputString) {
+  let wordNum = [];
+  let match;
+
+  while ((match = wordPattern.exec(inputString)) !== null) {
+    if (numberWords[match[0]]) {
+      wordNum.push(numberWords[match[0]]);
+    } else {
+      wordNum.push(match[0]);
+    }
+
+    wordPattern.lastIndex = match.index + 1;
+  }
+
+  return wordNum.toString().replaceAll(',', '');
+}
 
 function weatherMachineInterpreter() {
-  const numberPattern = new RegExp(/(\d)/g);
   let calibrationValue = 0;
 
   lines.forEach((line) => {
-    const digits = line.match(numberPattern);
+    const evalNum = wordToNumValue(line);
 
     switch (true) {
-      case digits?.length === 2:
-        calibrationValue += Number(`${digits[0]}${digits[1]}`);
+      case evalNum?.length >= 2:
+        calibrationValue += Number(
+          `${evalNum[0]}${evalNum[evalNum?.length - 1]}`
+        );
         break;
 
-      case digits?.length > 2:
-        calibrationValue += Number(`${digits[0]}${digits[digits?.length - 1]}`);
-        break;
-
-      case digits?.length < 2:
-        calibrationValue += Number(`${digits[0]}${digits[0]}`);
+      case evalNum.length === 1:
+        calibrationValue += Number(`${evalNum[0]}${evalNum[0]}`);
         break;
 
       default:
         console.log('No match found in:', line);
-        return;
+        break;
     }
   });
 
